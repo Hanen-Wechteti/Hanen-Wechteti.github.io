ed135e58dcb82897bde34fd4929b0946faef362c
Hanen Wechteti <hanenwechteti27@gmail.com>
2023-12-14 11:22:04 +0100

    finish

--- script/script.js
+++ script/script.js
@@ -5,26 +5,29 @@ let seriesData = [
         photo: "assets/images/vikings.jpg",
         title: "Vikings",
         creator: "Michael Hirst",
-        genre: "Série dramatique historique guerre",
+        genre: "Série historique guerre",
         date: 2013,
-        summary: "Ragnar Lothbrok et son frère Rollo participent à une bataille contre les peuples baltes.Après le massacre, Ragnar a des visions du dieu Odin et ses Valkyries.De retour dans son village, Ragnar se rend, en compagnie de son fils Björn, à Kattegat pour assister au Thing et afin que ce dernier subisse son rite de passage vers l'âge adulte."
+        summary: "Ragnar Lothbrok et son frère Rollo participent à une bataille contre les peuples baltes.Après le massacre, Ragnar a des visions du dieu Odin et ses Valkyries.",
+        trailer: "https://www.youtube.com/watch?v=mAl60ykBm4A",
     },
     {
         photo: "assets/images/virgin-river.webp",
         title: "Virgin River",
         creator: "Ian Hay",
-        genre: "Série dramatique romantique",
-        date: 2019,
-        summary: "Melinda « Mel » Monroe, infirmière en deuil, répond à une annonce pour travailler dans la ville reculée de Virgin River en Californie. Elle a tout abandonné à Los Angeles en pensant que ce serait l'endroit idéal pour repartir à zéro et laisser ses souvenirs douloureux derrière elle."
+        genre: "Série romantique",
+        date: "2019",
+        summary: "Melinda « Mel » Monroe, infirmière en deuil, répond à une annonce pour travailler dans la ville reculée de Virgin River en Californie. Elle a tout abandonné à Los Angeles en pensant que ce serait l'endroit idéal pour repartir à zéro et laisser ses souvenirs douloureux derrière elle.",
+        trailer: "https://www.youtube.com/watch?v=LnFHNsx3OkY",
     },
 
     {
         photo: "assets/images/good-witch.jpg",
         title: "Good Witch",
         creator: "Sue Tenney",
-        genre: "Serie Comédie dramatique fantastique",
+        genre: "Serie Comédie",
         date: 2015,
-        summary: "Jake Russel élève seul ses enfants Brandon et Laurie depuis son veuvage. Un jour, la femme du maire l'informe de l'arrivée de Cassandra à Grey House, un château abandonné et réputé hanté. Aussitôt, les commérages vont bon train sur la belle et mystérieuse étrangère"
+        summary: "Jake Russel élève seul ses enfants Brandon et Laurie depuis son veuvage. Un jour, la femme du maire l'informe de l'arrivée de Cassandra à Grey House, un château abandonné et réputé hanté. Aussitôt, les commérages vont bon train sur la belle et mystérieuse étrangère",
+        trailer: "https://www.youtube.com/watch?v=OLnxYaEYci8",
     },
     {
         photo: "assets/images/dark.jpg",
@@ -32,8 +35,8 @@ let seriesData = [
         creator: "Baran bo Odar",
         genre: "Science-fiction",
         date: 2015,
-        summary: "En 2019, le policier Ulrich Nielsen cherche désespérément son fils disparu, Mikkel, âgé de 11 ans. Trente-trois ans plus tôt, en 1986, c'est son frère cadet Mads qui avait disparu dans des circonstances tout aussi mystérieuses. "
-
+        summary: "En 2019, le policier Ulrich Nielsen cherche désespérément son fils disparu, Mikkel, âgé de 11 ans. Trente-trois ans plus tôt, en 1986, c'est son frère cadet Mads qui avait disparu dans des circonstances tout aussi mystérieuses. ",
+        trailer: "https://www.youtube.com/watch?v=VsLr5w_hM9c",
     },
     {
         photo: "assets/images/behind.jpeg",
@@ -41,43 +44,46 @@ let seriesData = [
         creator: "Steve Lightfoot",
         genre: "Thriller",
         date: 2021,
-        summary: "Louise est une jeune femme divorcée et mère d'un petit garçon, Adam. Un soir alors qu'une amie lui fait faux bond à un bar, elle rencontre un homme séduisant."
+        summary: "Louise est une jeune femme divorcée et mère d'un petit garçon, Adam. Un soir alors qu'une amie lui fait faux bond à un bar, elle rencontre un homme séduisant.",
+        trailer: "https://www.youtube.com/watch?v=c4LtoWQaLxk",
     },
     {
         photo: "assets/images/breaking.webp",
         title: "Breaking Bad",
         creator: "Vince Gilligan",
-        genre: "Drame-Policier-Thriller-Comédie noire",
+        genre: "Policier-Thriller",
         date: 2013,
-        summary: "Walter « Walt » White est professeur de chimie dans une école secondaire. Il vit à Albuquerque, au Nouveau-Mexique, avec son fils handicapé moteur et son épouse enceinte. Le lendemain de son cinquantième anniversaire,"
-
+        summary: "Walter « Walt » White est professeur de chimie dans une école secondaire. Il vit à Albuquerque, au Nouveau-Mexique, avec son fils handicapé moteur et son épouse enceinte. Le lendemain de son cinquantième anniversaire,",
+        trailer:"https://www.youtube.com/watch?v=dlnKTmdqxn4",
     },
     {
         photo: "assets/images/you.jpg",
         title: "You",
         creator: "Greg Berlanti",
         genre: "Thriller psychologique",
-        date: 2018,
-        summary: "Joe Goldberg, gérant d'une modeste librairie à New York. Un jour, il fait la rencontre d'une cliente, Guinevere Beck. La jeune femme est un véritable coup de foudre pour Joe qui décide de la retrouver sur Internet."
+        date: "2018",
+        summary: "Joe Goldberg, gérant d'une modeste librairie à New York. Un jour, il fait la rencontre d'une cliente, Guinevere Beck. La jeune femme est un véritable coup de foudre pour Joe qui décide de la retrouver sur Internet.",
+        trailer: "https://www.youtube.com/watch?v=sDOWgRpgc3w",
     },
 
     {
         photo: "assets/images/casa.jpg",
         title: "La casa de papel",
         creator: "Alex Pina",
-        genre: "Drame-Thriller-Braquage",
+        genre: "Thriller-Braquage",
         date: 2017,
-        summary: "Un homme mystérieux, surnommé le Professeur, planifie le meilleur braquage jamais réalisé. Pour exécuter son plan, il recrute huit des meilleurs malfaiteurs en Espagne qui n'ont rien à perdre"
-
+        summary: "Un homme mystérieux, surnommé le Professeur, planifie le meilleur braquage jamais réalisé. Pour exécuter son plan, il recrute huit des meilleurs malfaiteurs en Espagne qui n'ont rien à perdre",
+        trailer:"https://www.youtube.com/watch?v=rLKo9TCxv5Q",
     },
 
     {
         photo: "assets/images/emily.jpg",
         title: "Emily in Paris",
         creator: "Darren Star",
-        genre: "Comédie dramatique et romantique",
+        genre: "Comédie",
         date: 2020,
-        summary: " Emily Jane Cooper, une Américaine originaire de Chicago, accepte de déménager en France, et plus précisément à Paris, pour saisir une opportunité professionnelle. "
+        summary: " Emily Jane Cooper, une Américaine originaire de Chicago, accepte de déménager en France, et plus précisément à Paris, pour saisir une opportunité professionnelle. ",
+        trailer: "https://www.youtube.com/watch?v=oSkDB9xhFqU",
     },
 
     {
@@ -86,10 +92,18 @@ let seriesData = [
         creator: "Ronald D. Moore",
         genre: "Drame-Fantasy",
         date: 2014,
-        summary: "En 1945, une infirmière de guerre, Claire Randall, mariée à un descendant de capitaine des Dragons, Frank Randall, se retrouve transportée dans l'Écosse révoltée de 1743. Là-bas, elle va être immédiatement confrontée à un monde d'aventures qui lui est inconnu."
+        summary: "En 1945, une infirmière de guerre, Claire Randall, mariée à un descendant de capitaine des Dragons, Frank Randall, se retrouve transportée dans l'Écosse révoltée de 1743. Là-bas, elle va être immédiatement confrontée à un monde d'aventures qui lui est inconnu.",
+        trailer:"https://www.youtube.com/watch?v=0yzY6Rm-Fwg",
     }
-
 ];
+
+
+let header = document.querySelector('header');
+
+let h1 = document.createElement("h1");
+h1.className = "header";
+h1.textContent = "Mes top 10 Series";
+header.appendChild(h1);
 
 // Sélectionner le body du document
 const body = document.body;
@@ -114,10 +128,7 @@ seriesData.forEach((serie) => {
     title.className = "card_title";
     title.textContent = serie.title;
 
-    // Creer Header
-    let h1 = document.createElement("h1");
-    title.className = "header";
-    title.textContent = serie.header;
+   
 
     // Créer le créateur
     const creator = document.createElement("p");
@@ -138,6 +149,12 @@ seriesData.forEach((serie) => {
     const summary = document.createElement("p");
     summary.className = "summary";
     summary.textContent = serie.summary;
+   
+    // Anchor
+    let a = document.createElement('a');
+    a.className = 'trailer';
+    a.textContent = 'Bande annonce';
+    a.href = serie.trailer;
 
     // Ajouter les éléments à la div card
     card.appendChild(img);
@@ -146,7 +163,8 @@ seriesData.forEach((serie) => {
     card.appendChild(genre);
     card.appendChild(date);
     card.appendChild(summary);
-
+    card.appendChild(a);
     container.appendChild(card);
+
 });
 
